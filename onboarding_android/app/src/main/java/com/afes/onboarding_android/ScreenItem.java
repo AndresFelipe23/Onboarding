bcdc68e7c62e88f51b6a2c57b6b829528d8a37c4
Andres Espitia <75282613+AndresFelipe23@users.noreply.github.com>
2021-04-24 20:58:21 -0500

    Add files via upload

---
+++ onboarding_android/app/src/main/java/com/afes/onboarding_android/ScreenItem.java
@@ -0,0 +1,36 @@
+package com.afes.onboarding_android;
+
+public class ScreenItem {
+    String Tittle, Description;
+    int ScreenImg;
+
+    public ScreenItem(String tittle, String description, int screenImg){
+        Tittle = tittle;
+        Description = description;
+        ScreenImg = screenImg;
+    }
+
+    public String getTittle() {
+        return Tittle;
+    }
+
+    public void setTittle(String tittle) {
+        Tittle = tittle;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public int getScreenImg() {
+        return ScreenImg;
+    }
+
+    public void setScreenImg(int screenImg) {
+        ScreenImg = screenImg;
+    }
+}
